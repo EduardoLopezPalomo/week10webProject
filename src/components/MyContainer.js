69409c653514975e6e588eb53609114b0cac7e8b
Eduardo <e.lpalomo@alumnos.upm.es>
2024-01-28 12:54:41 +0200

    a

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MyList from './MyList';
 
 const MyContainer = () => {
-  const listHeader = "My List Header";
-  const listItems = [
-    { id: "1", text: "Item 1" },
-    { id: "2", text: "Item 2" },
-    { id: "3", text: "Item 3" },
-  ];
+    const [items, setItems] = useState([
+        { id: "1", text: "Item 1" },
+        { id: "2", text: "Item 2" },
+        { id: "3", text: "Item 3" },
+      ]);
+    const [newItemText, setNewItemText] = useState("");
+
+    const handleAddItem = () => {
+        const newItem = { id: Date.now().toString(), text: newItemText };
+        setItems(prevItems => [...prevItems, newItem]);
+        setNewItemText("");
+    };
   return (
     <div>
       <h2>This is MyContainer Component</h2>
-      <MyList header={listHeader} items={listItems} />
+      <MyList header="My List Header" items={items} />
+
+      <textarea
+        value={newItemText}
+        onChange={(e) => setNewItemText(e.target.value)}
+        placeholder="Enter new item text"
+      />
+
+      <button onClick={handleAddItem}>
+        Add New Item
+      </button>
     </div>
   );
 }
