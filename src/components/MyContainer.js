25bd1323be76a4160643c4cb65f29dd39e581566
Eduardo <e.lpalomo@alumnos.upm.es>
2024-01-28 12:38:23 +0200

    first commit

---
+++ src/components/MyContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyList from './MyList';
+
+const MyContainer = () => {
+  return (
+    <div>
+      <h2>This is MyContainer Component</h2>
+      <MyList />
+    </div>
+  );
+}
+
+export default MyContainer;
